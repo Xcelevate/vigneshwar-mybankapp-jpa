86780cf7b34e5a3fc23848fb10c8e433908d49a4
Vigneshwar B <Vigneshwar.2525@xcelevate.org>
2026-02-26 16:14:21 +0530

    Update Main.java

--- src/main/java/org/example/mybank/Main.java
+++ src/main/java/org/example/mybank/Main.java
@@ -2,8 +2,7 @@ package org.example.mybank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Welcome to viki's Bank" +
-                " ===");
+        System.out.println("=== Welcome to viki's Bank ===");
         ShowMenu showMenu = new ShowMenu();
         showMenu.entryPoint();
     }
